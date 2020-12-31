c3368a6bc01a617fa9bbd69a4be7ee437e6f23b9
masteraychen <858665768@qq.com>
2020-12-31 10:21:00 +0800

    refine CreateAccountDemo by adding more description

--- demo/src/main/java/CreateAccountDemo.java
+++ demo/src/main/java/CreateAccountDemo.java
@@ -9,21 +9,31 @@ public class CreateAccountDemo {
 
     public static void main(String[] args) {
 
-        EosRpcService eosRpcService = new EosRpcService("http://sh-test.vm.mgps.me:8888");
+        // MGP is the core asset/symbol for MangoChain
 
+        String newAccount = "mgpstestdemo"; //The name of the new account: [a-z,1-5] max 12 characters
+        String newAccountOwnerPubKey = "EOS6xt9rXurXqp7TTx4FkPff9A28BoZXbAS6aU2Rk1cEjJNx6icqr";
+        String newAccountActivePubKey = newAccountOwnerPubKey; //can choose a different one. using the same one is for simplicity.
+
+        String creator = "mgpheying111";    //The name of the account creating the new account
+        String creatorPrivKey = "5Jg9EGiVX7b8HSNyPGHVDTxKDwe4VusZhu4GDNZEmxJsovDKiFe";
         
-        
-        String ownerPubKey = "EOS6xt9rXurXqp7TTx4FkPff9A28BoZXbAS6aU2Rk1cEjJNx6icqr";
-        String activePubKey = ownerPubKey; //can choose a different one. using the same one is for simplicity.
-        String privKey = "5Jg9EGiVX7b8HSNyPGHVDTxKDwe4VusZhu4GDNZEmxJsovDKiFe";
-    
-        String creator = "mgpheying111";
-        String newAccount = "mgp.testdemo";
- 
+        String mgpTestnetRpcUrl = "http://sh-test.vm.mgps.me:8888";
+        EosRpcService eosRpcService = new EosRpcService( mgpTestnetRpcUrl );
+
+        long buyRamKbytes = 4300L;
+        String stakeNetAsset = "0.0004 MGP"; //The amount of tokens delegated for net bandwidth
+        String stakeCpuAsset = "0.0004 MGP"; //The amount of tokens delegated for CPU bandwidth
+        long delaySec = 0L; // set the delay_sec seconds, defaults to 0s
+
         try {
-            Transaction account = eosRpcService.createAccount(privKey, creator, newAccount, ownerPubKey, activePubKey, 4300L, "0.0004 MGP", "0.0004 MGP", 0L); 
+            Transaction account = eosRpcService.createAccount(privKey, creator, 
+                newAccount, newAccountOwnerPubKey, newAccountActivePubKey, 
+                buyRamKbytes, stakeNetAsset, stakeCpuAsset, delaySec);
+
         } catch (ApiException e) {
             System.err.println(e.getError().getError().getWhat());
+
         } catch (Exception e) {
             e.printStackTrace();
 
